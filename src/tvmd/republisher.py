e4866197d1d2667391444dad2bd20377dc23b70a
sciyen <a885566885566@gmail.com>
2023-12-05 14:14:33 +0800

    Add control allocation meta data visualization

--- src/tvmd/republisher.py
+++ src/tvmd/republisher.py
@@ -12,6 +12,8 @@ from sensor_msgs.msg import JointState
 
 # For meta data visualization
 from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
+from std_msgs.msg import ColorRGBA
 from visualization_msgs.msg import MarkerArray
 
 '''
@@ -51,7 +53,186 @@ def get_param(name, value=None):
         return rospy.get_param(name)
     else:
         return value
+
+class MetaDataMarker:
+    def __init_pseudo_forces(self) -> None:
+        for aid in range(self.num_modules):
+            # default settings
+            # https://docs.ros.org/en/noetic/api/visualization_msgs/html/msg/Marker.html
+            self.marker_array.markers[aid].header.frame_id = "module{}_body_link".format(aid+1)
+            self.marker_array.markers[aid].header.stamp = rospy.Time.now()
+            self.marker_array.markers[aid].ns = "module{}".format(aid+1)
+            self.marker_array.markers[aid].id = aid
+            self.marker_array.markers[aid].type = Marker.LINE_STRIP
+            self.marker_array.markers[aid].action = Marker.ADD
+            
+            self.marker_array.markers[aid].pose.position.x = 0
+            self.marker_array.markers[aid].pose.position.y = 0
+            self.marker_array.markers[aid].pose.position.z = 0
+            self.marker_array.markers[aid].pose.orientation.x = 0.0
+            self.marker_array.markers[aid].pose.orientation.y = 0.0
+            self.marker_array.markers[aid].pose.orientation.z = 0.0
+            self.marker_array.markers[aid].pose.orientation.w = 1.0
+
+            # For LineStrips: only scale.x is used and it controls the width of the line segments.
+            self.marker_array.markers[aid].scale.x = 0.01
+            self.marker_array.markers[aid].scale.y = 0.01
+            self.marker_array.markers[aid].scale.z = 0.01
+
+            # How long the object should last before being automatically deleted.  0 means forever
+            # self.marker_array.markers[aid].lifetime = 0.0
+            
+            # If this marker should be frame-locked, i.e. retransformed into its frame every timestep
+            self.marker_array.markers[aid].frame_locked = True
+
+            self.marker_array.markers[aid].points = [ Point() for i in range(self.num_modules+1) ]
+            self.marker_array.markers[aid].colors = [ ColorRGBA() for i in range(self.num_modules+1)]
+            for iter in range(self.num_modules):
+                self.marker_array.markers[aid].points[iter].x = 0.0
+                self.marker_array.markers[aid].points[iter].y = 0.0
+                self.marker_array.markers[aid].points[iter].z = iter / 10.0
+                self.marker_array.markers[aid].colors[iter].a = 1.0
+                self.marker_array.markers[aid].colors[iter].r = 1 - iter / (self.num_modules + 1)
+                self.marker_array.markers[aid].colors[iter].g = iter / (self.num_modules + 1)
+                self.marker_array.markers[aid].colors[iter].b = 0.0
+
+    @staticmethod
+    def __get_default_arrow(id) -> Marker:
+        marker = Marker()
+        marker.header.frame_id = "base_link"
+        marker.header.stamp = rospy.Time.now()
+        marker.ns = "wrench"
+        marker.id = id
+        marker.type = Marker.ARROW
+        marker.action = Marker.ADD
+        marker.pose.position.x = 0
+        marker.pose.position.y = 0
+        marker.pose.position.z = 0
+        marker.pose.orientation.x = 0.0
+        marker.pose.orientation.y = 0.0
+        marker.pose.orientation.z = 0.0
+        marker.pose.orientation.w = 1.0
+
+        # scale.x is the shaft diameter, and 
+        # scale.y is the head diameter. 
+        # If scale.z is not zero, it specifies the head length.
+        marker.scale.x = 0.01
+        marker.scale.y = 0.02
+        marker.scale.z = 0.0
+
+        marker.color.a = 1.0
+        marker.color.r = 1.0
+        marker.color.g = 0.0
+        marker.color.b = 1.0
+
+        marker.points = [Point(), Point()]
+        marker.points[0].x = 0.0
+        marker.points[0].y = 0.0
+        marker.points[0].z = 0.0
+        marker.points[1].x = 0.0
+        marker.points[1].y = 0.0
+        marker.points[1].z = 0.0
+        return marker
+
+
+    def __init__(self, _num_module: int) -> None:
+        self.scales = 0.1
+        self.num_modules = _num_module
+        self.marker_array = MarkerArray()
+
+        # Pseudo forces
+        self.marker_array.markers = [Marker() for i in range(self.num_modules)]
+        self.__init_pseudo_forces()
+
+        # Wrenches
+        # wf_idx: wrench force index
+        self.wf_idx = self.num_modules
+        self.marker_array.markers.append(Marker())
+        self.marker_array.markers[self.wf_idx] = MetaDataMarker.__get_default_arrow(5)
+
+        # wt_idx: wrench torque index
+        self.wt_idx = self.num_modules + 1
+        self.marker_array.markers.append(Marker())
+        self.marker_array.markers[self.wt_idx] = MetaDataMarker.__get_default_arrow(6)
+        self.marker_array.markers[self.wt_idx].color.r = 0.0
+        self.marker_array.markers[self.wt_idx].color.g = 1.0
+
+        #wfd_idx: desired wrench force index
+        self.wfd_idx = self.num_modules + 2
+        self.marker_array.markers.append(Marker())
+        self.marker_array.markers[self.wfd_idx] = MetaDataMarker.__get_default_arrow(7)
+        self.marker_array.markers[self.wfd_idx].color.a = 0.3
+
+        #wtd_idx: desired wrench torque index
+        self.wtd_idx = self.num_modules + 3
+        self.marker_array.markers.append(Marker())
+        self.marker_array.markers[self.wtd_idx] = MetaDataMarker.__get_default_arrow(8)
+        self.marker_array.markers[self.wtd_idx].color.a = 0.3
+        self.marker_array.markers[self.wtd_idx].color.r = 0.0
+        self.marker_array.markers[self.wtd_idx].color.g = 1.0
+
     
+    """
+    Update the positions of the pseudo-forces
+    """
+    def update_forces(self, f_x: list, f_y: list, f_z: list, sat_indices: list, d:list) -> None:
+        active_agent = [True for i in range(self.num_modules)]
+        c = 0
+        for iter in range(self.num_modules):
+            if sat_indices[iter] < 0 or sat_indices[iter] >= self.num_modules or c >= 1.0:
+                # End of allocation
+                active_agent = [False for i in range(self.num_modules)]
+
+            for aid in range(self.num_modules):
+                if active_agent[aid]:
+                    idx = self.get_idx(iter, aid)
+                    self.marker_array.markers[aid].points[iter+1].x = f_x[idx] * self.scales
+                    self.marker_array.markers[aid].points[iter+1].y = f_y[idx] * self.scales
+                    self.marker_array.markers[aid].points[iter+1].z = f_z[idx] * self.scales
+                else:
+                    self.marker_array.markers[aid].points[iter+1].x = self.marker_array.markers[aid].points[iter].x
+                    self.marker_array.markers[aid].points[iter+1].y = self.marker_array.markers[aid].points[iter].y
+                    self.marker_array.markers[aid].points[iter+1].z = self.marker_array.markers[aid].points[iter].z
+
+            c += d[iter]
+            
+            # Mark as saturated
+            if sat_indices[iter] >= 0 and sat_indices[iter] < self.num_modules:
+                active_agent[sat_indices[iter]] = False
+                
+    def __update_arrow(self, marker_idx: int, vec: list) -> None:
+        self.marker_array.markers[marker_idx].points[1].x = vec[0] * self.scales
+        self.marker_array.markers[marker_idx].points[1].y = vec[1] * self.scales
+        self.marker_array.markers[marker_idx].points[1].z = vec[2] * self.scales
+
+    def update_wrench(self, sp:list) -> None:
+        torque = sp[0:3]
+        force = sp[3:6]
+        self.__update_arrow(self.wf_idx, force)
+        self.__update_arrow(self.wt_idx, torque)
+
+    def update_desired_wrench(self, sp:list) -> None:
+        torque = sp[0:3]
+        force = sp[3:6]
+        self.__update_arrow(self.wfd_idx, force)
+        self.__update_arrow(self.wtd_idx, torque)
+
+    def get_idx(self, iter: int, agent: int) -> int:
+        assert iter >= 0 and iter < self.num_modules
+        assert agent >= 0 and agent < self.num_modules
+        return iter * self.num_modules + agent
+    
+    def pretty_print(self, data: ControlAllocationMetaData):
+        for aid in range(self.num_modules):
+            print("Agent {}".format(aid), end=': ')
+            for iter in range(self.num_modules):
+                idx = self.get_idx(iter, aid)
+                print("({:5.2f}, {:5.2f}, {:5.2f}) \t".format(
+                    data.f_x[idx], data.f_y[idx], data.f_z[idx]), end='')
+            print()
+        print()
+
+
 class Republisher:
     def __init_urdf(self, robot):
         # https://github.com/ros/joint_state_publisher/blob/noetic-devel/joint_state_publisher/src/joint_state_publisher/__init__.py
@@ -154,6 +335,10 @@ class Republisher:
 
         # TODO: Load parameters from urdf file, and calculate the scales for joints
 
+        topic = 'visualization_marker_array'
+        self.marker_pub = rospy.Publisher(topic, MarkerArray, queue_size=1)
+        self.marker_data = MetaDataMarker(self.num_modules)
+
     def data_update(self, data, mode):
         servo_offset = 0 if mode == "servos" else 2
         for i in range(4):
@@ -173,13 +358,6 @@ class Republisher:
                     self.joint_msg.position[joint_idx] = data.control[ctrl_idx] * factor + offset
                 else:
                     self.joint_msg.position[joint_idx] += data.control[ctrl_idx]
-
-            # if mode == "servos":
-            #     self.joint_msg.position[4*i+0] = data.control[2*i+0]
-            #     self.joint_msg.position[4*i+1] = data.control[2*i+1]
-            # else:
-            #     self.joint_msg.position[4*i+2] += data.control[2*i+0]
-            #     self.joint_msg.position[4*i+3] += data.control[2*i+1]
         
         upcoming_timestamp = data.timestamp
 
@@ -190,22 +368,34 @@ class Republisher:
 
         self.last_timestamp = data.timestamp
     
-    def motor_listener(self, data):        
+    def motor_listener(self, data: ActuatorMotors):        
         self.data_update(data, "motors")
     
-    def servo_listener(self, data):
+    def servo_listener(self, data: ActuatorServos):
         self.data_update(data, "servos")
     
-    def meta_data_listener(self, data):
-        pass
+    def meta_data_listener(self, data: ControlAllocationMetaData):
+        print(data.saturated_idx)
+        print(data.increment)
+        self.marker_data.pretty_print(data)
+        
+        # update marker data
+        self.marker_data.update_forces(data.f_x, data.f_y, data.f_z, data.saturated_idx, data.increment)
+        self.marker_data.update_wrench(data.allocated_control)
+        self.marker_data.update_desired_wrench(data.control_sp)
+
+        # publish marker topic
+        self.marker_pub.publish(self.marker_data.marker_array)
+
 
     def run(self):
         # Allow Rviz to initialize robot model
         hz = get_param("rate", 10)  # 10hz
         r = rospy.Rate(hz)
-        for k in range(50):
+        for k in range(10):
             self.joint_msg.header.stamp = rospy.Time.now()
             self.pub.publish(self.joint_msg)
+            self.marker_pub.publish(self.marker_data.marker_array)
             r.sleep()
         
         print("Start republishing")
